ebffef379eab693eb3c05e1a895a4c31f73a3b4f
x7368907 <108885586+x7368907@users.noreply.github.com>
2023-01-11 17:35:32 +0800

    Add files via upload

---
+++ vue第三週/js/products.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue;
+const url = "https://vue3-course-api.hexschool.io/v2";
+const path = "lee-ren";
+
+const app = {
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+}
+
+createApp(app).mount('#app')
